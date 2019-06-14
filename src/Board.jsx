92035010c599d2ea4d7dc0a73fad4d053650b55d
germa96@gmail.com <germa96@gmail.com>
2019-06-14 19:33:09 +0200

    Bottone board funzionante. Stringa in this.state.value

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import ReactFlagsSelect from 'react-flags-select';
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
-export default function Board() {
-  //  onSelectFlag();
+ class Board extends  React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    const countryCode= window.countryCode;
-    window.countryCode="IT";
-    return    <div>
-        <div className="demo-group">
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+    }
+     handleSubmit(event) {
+         alert('A name was submitted: ' + this.state.value);
+     }
+    render() {
+        return <div>
             <header className="header">
                 <h2>Scrivi o Parla!</h2>
             </header>
-            <textarea>
 
-            </textarea>
+         <TextField   value={this.state.value} onChange={this.handleChange} />
+            <Button variant="contained" color="primary" onClick={() => console.log(this.state.value)}
+             href="">
+                OK
+            </Button>
 
+        </div>
 
-    </div>
-    </div>
+    }
 }
+
+export default Board;
+
